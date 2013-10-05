1b67964a850d2a25a572090505e53c3a40a11970
Aday <Aday@PCA>
2013-10-05 13:13:07 +0100

    Fifth commit

--- src/frequencies/Frequencies.java
+++ src/frequencies/Frequencies.java
@@ -13,7 +13,8 @@ public class Frequencies {
         
         for (int i : vector) {
             if (map.containsKey(i)) {
-                map.put(i, ++(map.get(i)));
+                number=map.get(i);
+                map.put(i, ++number);
             }else map.put(i, 1);
         }
         
