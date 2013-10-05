5abfe6a2ed7b712702980400e599332a37977dec
Aday <Aday@PCA>
2013-10-05 13:11:02 +0100

    Third commit

--- src/frequencies/Frequencies.java
+++ src/frequencies/Frequencies.java
@@ -9,12 +9,12 @@ public class Frequencies {
         int [] vector = {0,1,0,2,0,3,0,4,0,1,0,5,0,6,0,1,0,2,7,3,4,5,6,7,8,9};
         int maxNumber=0;
         int maxTimes=0;
+        int number=0;
         
         for (int i : vector) {
             if (map.containsKey(i)) {
-                int number=map.get(i);
-                number++;
-                map.put(i, number);
+                number=map.get(i);
+                map.put(i, ++number);
             }else {
                 map.put(i, 1);
             }
